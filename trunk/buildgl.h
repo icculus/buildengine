74b7fc17d232c7b6b4d10cf6512cd7b2182e49f4
icculus <icculus@93e08484-711e-0410-a0a6-aab9f2357333>
2001-06-13 14:50:19 +0000

    More functions, and -ansi/-pedantic compliance.

--- trunk/buildgl.h
+++ trunk/buildgl.h
@@ -7,10 +7,12 @@
  *  (including this file) to BUILD.
  */
 
-// "Build Engine & Tools" Copyright (c) 1993-1997 Ken Silverman
-// Ken Silverman's official web site: "http://www.advsys.net/ken"
-// See the included license file "BUILDLIC.TXT" for license info.
-// This file IS NOT A PART OF Ken Silverman's original release
+/*
+ * "Build Engine & Tools" Copyright (c) 1993-1997 Ken Silverman
+ * Ken Silverman's official web site: "http://www.advsys.net/ken"
+ * See the included license file "BUILDLIC.TXT" for license info.
+ * This file IS NOT A PART OF Ken Silverman's original release
+ */
 
 #ifndef _INCLUDE_BUILDGL_H_
 #define _INCLUDE_BUILDGL_H_
@@ -21,13 +23,31 @@
 
 #include <GL/gl.h>
 
-extern const GLubyte* (*dglGetString)(GLenum name);
-extern void (*dglBegin)(GLenum mode);
-extern void (*dglEnd)(void);
-extern void (*dglClear)(GLbitfield mask);
-extern void (*dglClearColor)(GLclampf red, GLclampf green, GLclampf blue, GLclampf alpha);
+typedef const GLubyte* (*glGetString_t)(GLenum name);
+extern glGetString_t dglGetString;
 
-#endif  // !defined _INCLUDE_BUILDGL_H_
+typedef void (*glBegin_t)(GLenum mode);
+extern glBegin_t dglBegin;
 
-// end of buildgl.h ...
+typedef void (*glEnd_t)(void);
+extern glEnd_t dglEnd;
+
+typedef void (*glClear_t)(GLbitfield mask);
+extern glClear_t dglClear;
+
+typedef void (*glClearColor_t)(GLclampf red, GLclampf green, GLclampf blue, GLclampf alpha);
+extern glClearColor_t dglClearColor;
+
+typedef void (*glDrawPixels_t)(GLsizei width, GLsizei height, GLenum format, GLenum type, const GLvoid *pixels);
+extern glDrawPixels_t dglDrawPixels;
+
+typedef void (*glPixelMapfv_t)(GLenum map, GLint mapsize, const GLfloat *values);
+extern glPixelMapfv_t dglPixelMapfv;
+
+typedef void (*glPixelStorei_t)(GLenum pname, GLint param);
+extern glPixelStorei_t dglPixelStorei;
+
+#endif  /* !defined _INCLUDE_BUILDGL_H_ */
+
+/* end of buildgl.h ... */
 
