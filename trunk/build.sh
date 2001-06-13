6f4e5bd9878a7194a1aecfc8d713d1ac57a563e4
icculus <icculus@93e08484-711e-0410-a0a6-aab9f2357333>
2001-06-13 14:45:54 +0000

    Now allows for DLL versions of the engine.

--- trunk/build.sh
+++ trunk/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-export BUILD_WINDOWED=y BUILD_NOMOUSEGRAB=y
+export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
+export BUILD_WINDOWED=y
+export BUILD_NOMOUSEGRAB=y
+export BUILD_SDLDEBUG=-
 ./build $*
-
 
